7d94666559a0e4741b501055d6abbd6304f2e08b
donatPeter <peterdonat.bme@gmail.com>
2021-07-07 17:50:08 +0200

    initial commit

---
+++ apps/dairy-farm/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'dairy-farm',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/dairy-farm',
+};
